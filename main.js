0eef4722c503f5bbd65ae63b0cdd3d49d2e6b333
mehmetcanseyhan <mehmet.can.seyhan1@gmail.com>
2022-11-11 19:05:30 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+"use strict"
+/*
+var isim = 'mehmetcan';
+document.write('isim: '+isim);
+*/
+
+//her 1 saniyede bir icindeki fonksiyonu cagirir
+setInterval(setClock, 1000)
+
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
+
+function setClock() {
+  const currentDate = new Date()
+  const secondsRatio = currentDate.getSeconds() / 60
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+  setRotation(secondHand, secondsRatio)
+  setRotation(minuteHand, minutesRatio)
+  setRotation(hourHand, hoursRatio)
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setClock()
+
